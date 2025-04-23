3d89cc04c179817e1f7ca8ad1539a80d318e1934
Warren Wu <warrenwu2002@gmail.com>
2025-04-23 00:23:54 -0400

    n

--- Psyche Web-Based Rhythm Game/Assets/Scripts/CountDownTimer.cs
+++ Psyche Web-Based Rhythm Game/Assets/Scripts/CountDownTimer.cs
@@ -2,17 +2,23 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using System;
 
 public class CountDownTimer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
-    [SerializeField] float remainingTime;
+    [SerializeField] double remainingTime;
 
     void Update()
     {
-        remainingTime -= Time.deltaTime;
-        int minutes = Mathf.FloorToInt(remainingTime / 60);
-        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        remainingTime = (Math.Round(Manager.Instance.theSong.clip.length) - Math.Round(Manager.getAudioSourceTime())) ;
+        int minutes = Mathf.FloorToInt((float)(remainingTime / 60));
+        int seconds = Mathf.FloorToInt((float)(remainingTime % 60));
         timerText.text = string.Format("{0:00}:{1:00}",minutes,seconds);
+
+    }
+
+    public void Start()
+    {
     }
 }
